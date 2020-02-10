fe8d6d1da45673534e9d57afe7e1973787e5b21f
Phelix Ochieng <9phelix1@gmail.com>
2020-02-10 15:26:58 +0300

    Init

---
+++ src/com/ochibooh/software/architecture/design/patterns/Main.java
@@ -0,0 +1,8 @@
+package com.ochibooh.software.architecture.design.patterns;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD? Welcome to Software Architecture and Design Patterns in Java");
+        System.out.println("=========================================================================");
+    }
+}
